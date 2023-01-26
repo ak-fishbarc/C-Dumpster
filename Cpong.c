9e8beec0045ae646c432e23630e344edcce8fc06
ak-fishbarc <60137170+ak-fishbarc@users.noreply.github.com>
2023-01-26 12:29:32 +0000

    Update Cpong.c
    Working on map as text for now. It takes me some time as I need to learn about pointers and memory allocation.

--- Cpong.c
+++ Cpong.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+
 int start()
 {   
     
     char answer[10];
     // Newline character to simplify comparison.
     char option_s[10] = "--start\n";
-    
     printf("Hello, welcome to game of pong. If you would like to start a new \n\
-    game, please type in: \n \
---start \n");
+game, please type in: \n \
+--start \n \
+Type anything else to exit. \n");
     fgets(answer, 9, stdin);
     
     int equality = strcmp(answer, option_s);
@@ -24,6 +26,39 @@ int start()
     
 }
 
+// Intro into pointers and memory allocation:
+int **create_playing_field(int size_x, int size_y)
+{
+    
+    int **field;
+    field = malloc(size_x * sizeof(int * ));
+    for(int x = 0; x < size_x; x++)
+    {
+        field[x] = malloc(size_y * sizeof(int));
+        for(int i = 0; i < size_y; i++)
+        {
+            field[x][i] = 1;
+        }
+    }
+    
+    // Temporarily here for Debugging, etc. 
+    printf("%s", "\n");
+    for(int x = 0; x < size_x; x++)
+    {
+        for(int y = 0; y < size_y; y++)
+        {
+            printf("%d", field[x][y]);
+        }
+        
+        printf("%s", "\n");
+        
+    }
+
+    return field;
+    
+}
+
+
 int main()
 {
     
@@ -31,6 +66,7 @@ int main()
     if(option == 1)
     {
         printf("We are here");
+        create_playing_field(10, 10);
     } else {
         printf("We are there");
     }
